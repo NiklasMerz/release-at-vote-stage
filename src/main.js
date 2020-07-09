5340b97acd86415e55682f624314548b43c1f25a
Niklas Merz <niklasmerz@linux.com>
2020-07-09 20:35:35 +0200

    Format

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@
     distributed with this work for additional information
     regarding copyright ownership.  The ASF licenses this file
     to you under the Apache License, Version 2.0 (the
-    "License"); you may not use this file except in compliance
+    'License'); you may not use this file except in compliance
     with the License.  You may obtain a copy of the License at
     http://www.apache.org/licenses/LICENSE-2.0
     Unless required by applicable law or agreed to in writing,
     software distributed under the License is distributed on an
-    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
     KIND, either express or implied.  See the License for the
     specific language governing permissions and limitations
     under the License.
@@ -34,13 +34,15 @@ async function run() {
       issue_number: issueCtx.number
     });
     console.log(`Issue body: ${issue.data.body}`);
-    checkString = issue.data.body
+    checkString = issue.data.body;
     if (checkMessageForRelease(issue.data.body)) {
+      console.log('Close issue', issueCtx.number);
       // Close issues soley asking for release
-      await client.issues.close({
+      await client.issues.update({
         owner: issueCtx.owner,
         repo: issueCtx.repo,
-        issue_number: issueCtx.number
+        issue_number: issueCtx.number,
+        state: 'closed'
       });
     }
 
@@ -49,13 +51,13 @@ async function run() {
       repo: issueCtx.repo,
       issue_number: issueCtx.number
     });
-    console.log(comments)
+    console.log(comments);
     const lastComment = comments[comments.length];
-    console.debug("Last comment author", lastComment?.user.login);
+    console.debug('Last comment author', lastComment?.user.login);
     checkString = lastComment?.body;
 
-    console.log("Check body", checkString);
-    
+    console.log('Check body', checkString);
+
     if (checkMessageForRelease(checkString)) {
       console.log(`Adding message: ${message} to issue ${issueCtx.number}`);
       await client.issues.createComment({
@@ -72,7 +74,7 @@ async function run() {
 
 function checkMessageForRelease(checkString) {
   ///release/g.test(checkString) ||
-  return /npm/g.test(checkMessageForRelease)
+  return /npm/g.test(checkMessageForRelease);
 }
 
 run();
